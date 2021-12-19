0bbd98a8364037ab77b6bb25eb3e2930b6d90246
FrancoPagnotta <fpagnotta@mobydigital.com>
2021-12-19 18:29:48 -0300

    create service that get the data of an item by id

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,8 +14,16 @@ export class AppService {
   constructor(private _httClient: HttpClient, private  _router: Router) { }
 
   getListItems() {
-    return this._httClient.get(`${this.url}/posts?_page=0&_limit=10`);
+    return this._httClient.get(`${this.url}/posts?_page=0&_limit=10`)
+      .pipe(
+        map((res: any) => {
+          return res;
+        })
+      )
   }
   
+  getListItem(id: number) {
+    return this._httClient.get(`${this.url}/posts/${id}`);
+  }
 
 }
