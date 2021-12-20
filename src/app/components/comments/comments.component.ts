37a1dd61b1c899e240c7f4b7a89db05426300d9c
FrancoPagnotta <fpagnotta@mobydigital.com>
2021-12-20 00:29:50 -0300

    improve form validations

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -19,14 +20,24 @@ export class CommentsComponent implements OnInit {
 
   createForm() {
     this.form = this._formBuilder.group({
-      name: ['', [Validators.required, Validators.minLength(3)] ],
-      email: ['', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+[\.][a-z]{2,3}$")] ],
-      comment: ['', Validators.required],
+      name: [ '', [Validators.required, Validators.minLength(3)] ],
+      email: [ '', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+[\.][a-z]{2,3}$")] ],
+      comment: [ '', [Validators.required, Validators.minLength(10), Validators.maxLength(30)] ],
     });
+  }
+
+  inputStatus (inputName: string) {
+    return this.form.get(inputName)?.invalid && this.form.get(inputName)?.touched;
   }
 
   save() {
     console.log(this.form);
+    
+    if (this.form.invalid) {
+      Object.values(this.form.controls).forEach(input => input.markAllAsTouched());
+    }
+
+    return;
   }
 
 }
