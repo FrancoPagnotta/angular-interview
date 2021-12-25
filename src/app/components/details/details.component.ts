fa5574380b5a7f67c87ee4afe664e2c6ccba4648
FrancoPagnotta <fpagnotta@mobydigital.com>
2021-12-25 01:21:41 -0300

    implements redirection to 404 when entering a non-existent id parameter

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, Output } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Post } from 'src/app/models/post';
 import { AppService } from 'src/app/services/app.service';
 
@@ -18,13 +18,18 @@ export class DetailsComponent implements OnInit {
 
   constructor(private _activatedRoute: ActivatedRoute,
               private _appService: AppService,
-              private _cdRef:ChangeDetectorRef) {}
+              private _cdRef:ChangeDetectorRef,
+              private _router: Router) {}
               
   ngOnInit(): void {
 
     this._activatedRoute.params.subscribe((res) => {
-      this.postId = res.id;
-      this.getItem(this.postId);
+      if (res.id > 0 && res.id <= 10) {
+        this.postId = res.id;
+        this.getItem(this.postId);
+      } else {
+        this._router.navigateByUrl('/404');
+      }
     });
 
     this._appService.getComments(this.postId);
